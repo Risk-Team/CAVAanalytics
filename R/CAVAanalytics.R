778d2b2501c63e4aed90b02710679d6917045b2a
Riccardo Soldan <riccardosoldan@hotmail.it>
2024-03-02 17:05:45 +0000

    updated Index

--- R/CAVAanalytics.R
+++
@@ -1,9 +0,0 @@
-#' CAVAanalytics Package
-#'
-#' Operational R package for climate services
-#' @docType package
-#'
-#' @author Riccardo Soldan \email{riccardosoldan@hotmail.it}
-#'
-#' @name CAVAanalytics
-NULL
